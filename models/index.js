26d388b449396269a0ff5152c80d6ba7662b23f8
Jeff Cameron <jeffgcameron@gmail.com>
2021-04-04 21:50:24 -0600

    Category and product get/post/delete

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 // import models
 const Product = require('./Product');
-const Category = require('./category');
+const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: 'CASCADE',
 })
 
 // Categories have many Products
@@ -17,19 +18,24 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
-})
-
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany( Product, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
   through: {
     model: ProductTag,
     unique: false
   },
-  as: "tag_product"
+  as: "ptag"
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany( Product, {
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: "protag"
 })
 
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,8 +3,8 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+//Find All Catagories
 router.get('/', async (req, res) => {
-  // find all categories
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
@@ -17,13 +17,44 @@ router.get('/', async (req, res) => {
 
 });
 
+
+//Find Category by ID
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      // JOIN with travellers, using the Trip through table
-      // include: [{ model: Product }]
+      include: [ {model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
     });
 
     if (!categoryData) {
@@ -35,18 +66,6 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-});
-
-router.post('/', (req, res) => {
-  // create a new category
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
 });
 
 module.exports = router;
